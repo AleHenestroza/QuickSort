b669377923e24cc0b30bdd6fe8d52b99dc8459c6
Alejandro Henestroza <alehenestroza@gmail.com>
2022-01-17 21:11:04 -0300

    Code refactoring and added README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+# A QuickSort implementation in Java
--- src/ar/com/alehenestroza/Main.java
+++ src/ar/com/alehenestroza/Main.java
@@ -5,32 +5,44 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        // Create array of 10 to 20 elements to sort
-        Random rand = new Random();
-        int[] numbers = new int[rand.nextInt(10, 20)];
+        int[] numbers = new int[1000000];
 
         // Populate the array with numbers from 0 to 100
+        Random rand = new Random();
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = rand.nextInt(100);
+            numbers[i] = rand.nextInt(10000);
         }
 
         System.out.println("Before:");
         printArray(numbers);
 
-        quicksort(numbers, 0, numbers.length - 1);
+        quicksort(numbers);
 
         System.out.println("\nAfter:");
         printArray(numbers);
     }
 
+    private static void quicksort(int[] array) {
+        quicksort(array, 0, array.length - 1);
+    }
+
     private static void quicksort(int[] array, int lowIndex, int highIndex) {
         if (lowIndex >= highIndex) return; // If array has only one element, return
 
-        int pivot = array[highIndex];
+        int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
+        int pivot = array[pivotIndex];
+        swap(array, pivotIndex, highIndex);
+
+        int leftPointer = partition(array, lowIndex, highIndex, pivot);
+
+        // Recursively sort the partitions
+        quicksort(array, lowIndex, leftPointer - 1); // Left partition
+        quicksort(array, leftPointer + 1, highIndex); // Right partition
+    }
+
+    private static int partition(int[] array, int lowIndex, int highIndex, int pivot) {
         int leftPointer = lowIndex;
         int rightPointer = highIndex;
-
-        // Partitioning
         while (leftPointer < rightPointer) {
             while (array[leftPointer] <= pivot && leftPointer < rightPointer) {
                 leftPointer++;
@@ -41,10 +53,7 @@ public class Main {
             swap(array, leftPointer, rightPointer);
         }
         swap(array, leftPointer, highIndex);
-
-        // Recursively sort the partitions
-        quicksort(array, lowIndex, leftPointer - 1); // Left partition
-        quicksort(array, leftPointer + 1, highIndex); // Right partition
+        return leftPointer;
     }
 
     private static void swap(int[] array, int index1, int index2) {
